f0629643c605409ecd2f9d2abb4528eeba6d3c56
julianortega <brutalrider1@gmail.com>
2023-10-04 01:00:49 +0200

    fix css and optimize img loading

--- src/components/CountriesList.tsx
+++ src/components/CountriesList.tsx
@@ -9,12 +9,12 @@ export function CountriesList ({ countries }: Props): JSX.Element {
   return (
     <div className="countries-list">
       {countries.map((country, index) => (
-        <a className="country" key={index} href={`https://en.wikipedia.org/wiki/${country.name.common}`}>
-          <img className="flag" src={country.flags.png} alt={country.name.common} />
+        <a className="country-card" key={index} href={`https://en.wikipedia.org/wiki/${country.name.common}`}>
+          <img className="country__flag" src={country.flags.png.replace('.png', '.jpg').replace('w320','w40')} alt={country.name.common} />
           <h3 className="country__name">{country.name.common}</h3>
           <p className="country__capital"><strong>Capital:</strong> {country.capital}</p>
           <p className="country__region"><strong>Region:</strong> {country.region}</p>
-          <p className="country__population"><strong>Population:</strong> {country.population}</p>
+          <p className="country__population"><strong>Population:</strong> {country.population.toLocaleString()}</p>
         </a>
       ))}
     </div>
