2927096bcbe03a178ea31ca79883a981258179f8
julianortega <brutalrider1@gmail.com>
2023-10-04 02:21:49 +0200

    fix pagination button

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -21,11 +21,13 @@ export const Pagination: React.FC<Props> = ({ page, pageCount, onPageChange }) =
       <button onClick={handlePrevPage} disabled={page === 1} className='previous-page'>
         Prev
       </button>
+      <div className='select-page-div'>
       {Array.from({ length: pageCount }).map((_, i) => (
         <button className="select-page" key={i} onClick={() => onPageChange(i + 1)} disabled={page === i + 1}>
           {i + 1}
         </button>
       ))}
+      </div>
       <button onClick={handleNextPage} disabled={page === pageCount} className='next-page'>
         Next
       </button>
